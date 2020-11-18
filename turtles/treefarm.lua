91933b23a6f9538b216346299d190a4c6b40476c
Max Klein <max.klein@grandcentrix.net>
2020-11-18 21:19:12 +0100

    first real try

--- turtles/treefarm.lua
+++ turtles/treefarm.lua
@@ -1,13 +1,68 @@
 os.loadAPI('apis/move')
 
+local row = 5
+local column = 5
+
+
+local function main()
+    startUp()
+end
+
+local farmTrees()
+    move.up()
+    move.forward()
+    move.forward()
+    move.forward()
+
+    local i = 0
+    while i < row do
+        i = i + 1
+        local j = 0
+        while j < column do
+            j = j + 1
+            move.forward()
+            move.forward()
+            if turtle.detect() then
+                cutTree()
+            else 
+                move.forward()
+                turtle.placeDown()
+            end
+        end
+        move.forward()
+        if i % 2 = 0 then
+            move.turnLeft()
+            move.forward()
+            move.forward()
+            move.forward()
+            move.turnLeft()
+        else
+            move.turnRight()
+            move.forward()
+            move.forward()
+            move.forward()
+            move.turnRight()
+        end
+    end
+end
+
+local function startUp()
+    if not (move.x == 0 and move.y == 0 and move.z == 0) then
+        returnHome()
+    end
+    readyUp()
+end
+
 local function cutTree()
+    turtle.dig()
+    move.forward()
+    turtle.digDown()
     while turtle.detectUp() do
         turtle.digUp()
         turtle.up()
     end
-    while not turtle.detectDown() do
-        turtle.down()
-    end
+    move.toZ(1)
+    turtle.placeDown()
 end
 
 local function unload()
@@ -43,7 +98,4 @@ local function readyUp()
     move.turnRight()
 end
 
-move.toX(10)
-move.toY(15)
-returnHome()
-readyUp()
+main()
