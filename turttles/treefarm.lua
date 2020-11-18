aa846ef3f5da92908df0aef32c7d61366941f448
Max Klein <max.klein@grandcentrix.net>
2020-11-18 16:03:26 +0100

    added treefram basics

--- turttles/treefarm.lua
+++ turttles/treefarm.lua
@@ -1,11 +1,50 @@
-while turtle.detectUp() do
-    turtle.digUp()
-    turtle.up()
-end
-while not turtle.detectDown() do
-    turtle.down()
+os.loadAPI('apis/move')
+
+move.toX(10)
+move.toY(15)
+returnHome()
+readyUp()
+
+function cutTree()
+    end
+    while turtle.detectUp() do
+        turtle.digUp()
+        turtle.up()
+    end
+    while not turtle.detectDown() do
+        turtle.down()
+    end
 end
 
-local savePos = fs.open('saves/treefarm/pos', 'w')
-savePos.writeLine('test')
-savePos.close()
+function unload()
+    local i = 3
+    while i <= 16 do
+        turtle.select(i)
+        turtle.drop()
+        i = i + 1
+    end
+end
+
+function returnHome()
+    while not move.toZ(20) do
+        turtle.digUp()
+    end
+    move.toX(0)
+    move.toY(0)
+    move.toZ(0)
+end
+
+function readyUp()
+    move.faceDirection(2)
+    unload()
+    move.turnLeft()
+    turtle.select(1)
+    turtle.suck()
+    move.turnLeft()
+    move.turnLeft()
+    turtle.select(2)
+    turtle.suck()
+    local amount = turtle.getItemCount() - 1
+    turtle.refuel(amount)
+    move.turnRight()
+end
