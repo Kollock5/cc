08527958c195de867f0e2bb711548fabe0ad93a0
Kollock <maxklein96@web.de>
2020-11-24 20:50:39 +0100

    uff

--- turtles/treefarm.lua
+++ turtles/treefarm.lua
@@ -91,7 +91,7 @@ local function farmTrees()
 end
 
 local function startUp()
-    print(move.x .. move.y .. move.z)
+    print(move.x .. move.y .. move.z .. 'test')
     if not (move.x == 0 and move.y == 0 and move.z == 0) then
         returnHome()
     end
