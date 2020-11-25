054a4a0debcfee8f90e6b1719f25cdec847d1884
Max Klein <max.klein@grandcentrix.net>
2020-11-26 00:36:01 +0100

    added new file

---
+++ turtles/farm.lua
@@ -0,0 +1,17 @@
+os.loadAPI('apis/move')
+
+local length = 8
+local width = 8
+
+local function farm()
+
+end
+
+function unload()
+    
+end
+
+function returnHome()
+    move.toX(0)
+    move.toY(0)
+end
