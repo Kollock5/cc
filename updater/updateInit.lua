15d67014ce171534e2fe7d0850561fdec05e516b
Max Klein <max.klein@grandcentrix.net>
2020-11-26 00:51:47 +0100

    updater

---
+++ updater/updateInit.lua
@@ -0,0 +1,32 @@
+function updateMove()
+    file = fs.open('up/upMove', 'w')
+    file.writeLine('shell.run("rm apis/move")')
+    file.writeLine('shell.run("wget https://raw.githubusercontent.com/Kollock5/cc/main/apis/move.lua apis/move")')
+    file.close()
+end
+
+function updateTree()
+    file = fs.open('up/upTree', 'w')
+    file.writeLine('shell.run("rm turtles/tree")')
+    file.writeLine('shell.run("wget https://raw.githubusercontent.com/Kollock5/cc/main/turtles/tree.lua turtles/tree")')
+    file.close()
+end
+
+function updateFarm()
+    file = fs.open('up/upFarm', 'w')
+    file.writeLine('shell.run("rm turtles/farm")')
+    file.writeLine('shell.run("wget https://raw.githubusercontent.com/Kollock5/cc/main/turtles/farm.lua turtles/farm")')
+    file.close()
+end
+
+function updateMine()
+    file = fs.open('up/upMine', 'w')
+    file.writeLine('shell.run("rm turtles/mine")')
+    file.writeLine('shell.run("wget https://raw.githubusercontent.com/Kollock5/cc/main/turtles/mine.lua turtles/farm")')
+    file.close()
+end
+
+updateMove()
+updateTree()
+updateFarm()
+updateMine()
